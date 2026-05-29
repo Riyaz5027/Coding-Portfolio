c7dc64e91f459bb6efbc21ec55529d35adc8efe9
Riyaz <riyazshaik5027@gmail.com>
2026-05-29 21:45:48 +0530

    Optimal code for find_pivot_index

--- Prefix_Sum_Problems/Find_Pivot_index.cpp
+++ Prefix_Sum_Problems/Find_Pivot_index.cpp
@@ -5,38 +5,37 @@ using namespace std;
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int n = nums.size();
 
-        vector<int> pre;
-        int sum = 0;
+        int totalSum = 0;
 
-        pre.push_back(0);
+        for (int num : nums)
+            totalSum += num;
 
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            pre.push_back(sum);
+        int leftSum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if (leftSum == rightSum)
+                return i;
+
+            leftSum += nums[i];
         }
 
-        int ind = -1;
-        int sp = pre.size();
-
-        for (int i = 1; i < sp; i++) {
-            if (pre[sp - 1] - pre[i] == pre[i - 1]) {
-                ind = i - 1;
-                break;
-            }
-        }
-
-        return ind;
+        return -1;
     }
 };
 
 int main() {
+
     vector<int> nums = {1, 7, 3, 6, 5, 6};
 
     Solution obj;
 
-    cout << obj.pivotIndex(nums);
+    int ans = obj.pivotIndex(nums);
+
+    cout << "Pivot Index = " << ans << endl;
 
     return 0;
 }
